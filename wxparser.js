b9cb168dc31224ebfd9fe73ed8a5c0a944c3fa16
alesh <ales@outlook.com>
2019-03-31 23:23:42 -0700

    parse timestamp

--- wxparser.js
+++ wxparser.js
@@ -1,11 +1,42 @@
 var wxParser = (function () {
-    // construct the thing
+    // -------------------------------------------------------
+    // constructor
+    // -------------------------------------------------------
     function wxParser(packet) {
         this.packet = '';
+        this.wxInfo = {};
         this.packet = packet;
         this.parse();
     }
+    // -------------------------------------------------------
+    // parse - root parser function
+    // -------------------------------------------------------
     wxParser.prototype.parse = function () {
+        var msg = this.packet.split('@');
+        var body = msg[1];
+        body = this.getTimeStamp(body);
+    };
+    // -------------------------------------------------------
+    // getTimeStamp - parses out the timestamp from the packet
+    // -------------------------------------------------------
+    wxParser.prototype.getTimeStamp = function (body) {
+        // let re = new RegExp('^((\d{6})(.))$')
+        var re = new RegExp(/^(\d{2})(\d{2})(\d{2})(.)(.*$)/);
+        var match = re.exec(body);
+        if (match) {
+            var now = new Date();
+            if (match[4] == 'z') {
+                // convert this into UTC Unix timestamp
+                var ts = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), parseInt(match[1]), // DD
+                parseInt(match[2]), // hh
+                parseInt(match[3]), // mm
+                0) / 1000; // ss
+                this.wxInfo.timestamp = ts;
+                // return the last group (the remainder of the body)
+                return match[5];
+            }
+        }
+        console.error("Unsupported time format!");
     };
     return wxParser;
 }());
--- wxparser.ts
+++ wxparser.ts
@@ -1,15 +1,48 @@
 
 
 class wxParser {
-  packet:string = '';
+  private packet:string = '';
+  private wxInfo : any = {}
 
-  private parse() {
-
-  }
-
-  // construct the thing
+  // -------------------------------------------------------
+  // constructor
+  // -------------------------------------------------------
   constructor(packet:string) {
     this.packet = packet;
     this.parse();
+  }
+
+  // -------------------------------------------------------
+  // parse - root parser function
+  // -------------------------------------------------------
+  private parse() {
+    let msg = this.packet.split('@');
+    let body : string = msg[1];
+    body = this.getTimeStamp(body);
+  }
+
+  // -------------------------------------------------------
+  // getTimeStamp - parses out the timestamp from the packet
+  // -------------------------------------------------------
+  getTimeStamp(body : string) :string {
+    // let re = new RegExp('^((\d{6})(.))$')
+    let re = new RegExp(/^(\d{2})(\d{2})(\d{2})(.)(.*$)/);
+    let match = re.exec(body);
+    if(match) {
+      let now = new Date();
+      if(match[4] == 'z') {
+        // convert this into UTC Unix timestamp
+        let ts = Date.UTC(now.getUTCFullYear(), 
+                          now.getUTCMonth(), 
+                          parseInt(match[1]), // DD
+                          parseInt(match[2]), // hh
+                          parseInt(match[3]), // mm
+                          0) / 1000;          // ss
+        this.wxInfo.timestamp = ts;
+        // return the last group (the remainder of the body)
+        return match[5];
+      }      
+    }
+    console.error("Unsupported time format!");
   }
 }
